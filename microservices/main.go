6d5a20378ca3bdc813a517512c137f18bc61a2ad
Steven Truong <>
2022-11-04 16:52:02 +0000

    wip microservices p2

--- microservices/main.go
+++ microservices/main.go
@@ -2,14 +2,25 @@ package main
 
 import (
 	"log"
+	"microservices/handlers"
 	"net/http"
+	"os"
 )
 
 func main() {
-	http.HandleFunc("/", func(_ http.ResponseWriter, _ *http.Request) {
-		log.Println("Hello World")
-	})
+	logger := log.New(os.Stdout, "product-api", log.LstdFlags)
+
+	helloHandler := handlers.NewHello(logger)
+	goodbyeHandler := handlers.NewGoodbye(logger)
+
+	serveMux := http.NewServeMux()
+	// Register a handler to path /
+	serveMux.Handle("/", helloHandler)
+	serveMux.Handle("/goodbye", goodbyeHandler)
+
+	server := new http.Server{}
+
 
 	// create a web service, bind to every address on my machine to port 9090
-	http.ListenAndServe(":9090", nil)
+	http.ListenAndServe(":9090", serveMux)
 }
